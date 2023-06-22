4938ae457bff521aa55d14915d1367477df045f3
TheRunaway <shadowangel404@proton.me>
2023-06-22 15:48:52 +0200

    Delete WorldEdit directory

--- WorldEdit/commands/kill.py
+++
@@ -1,8 +0,0 @@
-# Created by Wallee#8314/Red-exe-Engineer
-
-# Imports
-from mcpi.minecraft import Minecraft
-mc = Minecraft.create()
-
-# Set the user's position to the void, instant death
-mc.player.setPos(0, -1024, 0)
